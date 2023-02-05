0653ebbe7096b4727ab81bd8b06501e65af80326
ayhanhacioglu@gmail.com <ayhanhacioglu@gmail.com>
2023-02-06 01:34:56 +0300

    Rating Kolonu eklendi

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -4,19 +4,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
-  public class Movie
-  {
-    public int ID { get; set; }
-    
-    public string? Title { get; set; }
+    public class Movie
+    {
+        public int ID { get; set; }
 
-    [DataType(DataType.Date)]
-    [DisplayName("Release Date")]
-    public DateTime ReleaseDate { get; set; }
-    
-    public string? Genre { get; set; }
-    
-    [Column(TypeName = "decimal (18,2)")]
-    public decimal Price { get; set; }
-  }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        public string Rating { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal (18,2)")]
+        public decimal Price { get; set; }
+    }
 }
