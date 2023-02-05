160320d71b1427429dcbe7fbaf688b90c208c09f
ayhanhacioglu@gmail.com <ayhanhacioglu@gmail.com>
2023-02-05 17:06:31 +0300

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+  public class Movie
+  {
+    public int ID { get; set; }
+    
+    public string? Title { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayName("Release Date")]
+    public DateTime ReleaseDate { get; set; }
+    
+    public string Genre { get; set; }
+    
+    [Column(TypeName = "decimal (18,2)")]
+    public decimal Price { get; set; }
+  }
+}
